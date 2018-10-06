c2ce993e74c61ad18243970aafc6ebced6eb94ab
Danny <danny.12343@hotmail.co.uk>
2018-10-06 21:41:42 +0100

    Move settings fragment out

---
+++ Snapshot.App/SettingsFragment.cs
@@ -0,0 +1,13 @@
+﻿using Android.OS;
+using Android.Support.V7.Preferences;
+
+namespace Snapshot.App
+{
+    public class SettingsFragment : PreferenceFragmentCompat
+    {
+        public override void OnCreatePreferences(Bundle savedInstanceState, string rootKey)
+        {
+            AddPreferencesFromResource(Resource.Xml.prefs);
+        }
+    }
+}
